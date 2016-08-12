98fb923c2b214e01be87412f353d289461af803f
Tom Reeb <Thomas_Reeb@comcast.com>
2016-08-12 16:38:42 -0400

    changes

--- modules/xwifi_spacewalk/manifests/init.pp
+++ modules/xwifi_spacewalk/manifests/init.pp
@@ -1,7 +1,7 @@
 class xwifi_spacewalk {
 
   $spwk_svr = xfwspwk-ch2-02.sys.comcast.net
-  $spwk_pkgs = [ 'rhn-client-tools', 'rhn-check', 'rhn-setup', 'rhnsd', 'm2crypto', 'yum-rhn-plugin', 'rhncfg', 'rhncfg-actions', 'rhncfg-client', 'osad' ]
+  $spwk_pkgs = [ 'rhn-client-tools', 'rhn-check', 'rhn-setup', 'rhnsd', 'm2crypto', 'yum-rhn-plugin', 'rhncfg', 'rhncfg-actions', 'rhncfg-client', 'osad', 'python-hwdata' ]
 
   if ($::fqdn =~ /^xfwaaa/) {
     $activation_key = 1-xfinity_wifi_3xA
@@ -16,6 +16,13 @@ class xwifi_spacewalk {
     $activation_key = 1-generic
   }
 
+  yumrepo { "spwk_repo":
+      baseurl => "http://yum.spacewalkproject.org/2.4-client/RHEL/$operatingsystemmajrelease/$architecture",
+      descr => "Spacewalk 2.4 Client Repo",
+      enabled => 1,
+      gpgcheck => 0
+   }
+
   package { $spwk_pkgs:
     ensure  => 'latest',
   }
@@ -26,21 +33,29 @@ class xwifi_spacewalk {
     require => Package['osad'];
     }
 
-  exec{'retrieve_cert':
-    command => "/usr/bin/wget -P /usr/share/rhn http://$spwk_svr/pub/RHN-ORG-TRUSTED-SSL-CERT",
+  exec{ 'repo_del' :
+    path    => '/bin',
+    command => "mv /etc/yum.repos.d /etc/yum.repos.d.bak",
+  }
+
+  exec{ 'retrieve_cert' :
+    path    => '/usr/bin',
+    command => "wget -P /usr/share/rhn http://$spwk_svr/pub/RHN-ORG-TRUSTED-SSL-CERT",
     creates => "/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT",
   }
 
-  file{'/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT':
+  file{ '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT' :
     mode    => 0755,
     require => Exec["retrieve_cert"],
   }
 
-  exec{'activate':
-    command => "rhnreg_ks --serverUrl=http://$spwk_svr/XMLRPC --activationkey=$activation_key"
+  exec{ 'activate' :
+    path    => '/usr/sbin',
+    command => "rhnreg_ks --serverUrl=http://$spwk_svr/XMLRPC --activationkey=$activation_key",
   }
 
-  exec{'rhn-actions':
-    command => "rhn-actions-control --enable-all"
+  exec{ 'rhn-actions' :
+    path    => '/usr/sbin',
+    command => "rhn-actions-control --enable-all",
   }
 }
